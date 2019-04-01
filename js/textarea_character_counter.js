c04410996651b361810ffb1bae099ebcd7b17a52
Nathalia <nath.campreguer@gmail.com>
2019-04-01 14:09:18 -0600

    Textarea input with character count

---
+++ js/textarea_character_counter.js
@@ -0,0 +1,18 @@
+(function (window, document, undefined) {
+
+  'use strict';
+
+  var text_input = document.getElementById('input_text');
+
+  function countCharacters(e) {
+    var text_entered, counter, count_remaining;
+    count_remaining = document.getElementById('characters_remaining');
+
+    text_entered = this.value;
+    counter = (300 - (text_entered.length));
+    count_remaining.textContent = counter;
+  }
+
+  text_input.addEventListener('keyup', countCharacters, false);
+
+})(window, document);
