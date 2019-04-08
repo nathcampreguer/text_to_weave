2e50fee3b1b0d88416b54e83e07da5ec3998e44d
Nathalia <nath.campreguer@gmail.com>
2019-04-08 13:06:21 -0600

    Adjust canva width to user screen display (doesn't account for screens smaller than the raft width) and adjust canva height to draft height.

--- js/text_to_weave.js
+++ js/text_to_weave.js
@@ -6,6 +6,7 @@
   var text_input = document.getElementById('input_text');
   const canvas = document.getElementById("canvas");
   const context = canvas.getContext('2d');
+  // const gl = canvas.getContext("webgl");
 
 
   function encode_text() {
@@ -15,11 +16,18 @@
     var row = 0;
 
     context.clearRect( 0, 0, context.canvas.width, context.canvas.height);
+    resize(draft.length);
 
     for (let draft_line of draft) {
       draw_to_canvas(draft_line, row);
       row++;
     }
+
+    var dataURL = canvas.toDataURL("image/png");
+    var link = document.getElementById("download_link");
+
+    link.href = dataURL;
+    link.title = "weave_draft";
   }
 
   function draw_to_canvas(draft_line, row) {
@@ -34,6 +42,19 @@
       context.fillRect(col, row, 1, 1);
       col++;
     }
+  }
+
+  function resize(draft_height) {
+    // Lookup the width size the browser is displaying the canvas.
+    var displayWidth  = canvas.clientWidth;
+
+    // Check if the canvas is not the same size.
+    if (canvas.width  != displayWidth) {
+
+      // Make the canvas the same size
+      canvas.width  = displayWidth;
+    }
+    canvas.height = draft_height;
   }
 
   //events
